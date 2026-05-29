996660cc20ef50de81bf606b3a521e608f21e629
ブルー <98825268+nOjms-Blue@users.noreply.github.com>
2026-05-29 20:33:48 +0900

    remove: program gateway in main.go

--- main.go
+++ main.go
@@ -3,77 +3,11 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"sync"
 	
-	"bakashier/cli"
 	"bakashier/constants"
-	"bakashier/core"
-	"bakashier/view"
 )
 
 
 func main() {
-	args, err := cli.ParseArgs(os.Args[1:])
-	if err != nil {
-		fmt.Println(err.Error())
-		cli.Usage()
-		os.Exit(1)
-	}
-	
-	settings := core.Settings{
-		SrcDir: args.SrcDir,
-		DistDir: args.DistDir,
-		Password: args.Password,
-		Workers: args.Workers,
-		ChunkSize: args.ChunkSize,
-		Limit: core.SettingsLimit{Size: args.LimitSize, Wait: args.LimitWait},
-	}
-	run := func() {
-		if settings.Password == "" {
-			input, err := cli.InputPassword()
-			if err != nil {
-				fmt.Println(err.Error())
-				os.Exit(1)
-			}
-			settings.Password = input
-		}
-		
-		wg := sync.WaitGroup{}
-		toViewQueue := make(chan view.MessageToView, 64)
-		toManagerQueue := make(chan view.MessageToManager, 64)
-		
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			model, err := view.Run(args.Mode, toViewQueue, toManagerQueue)
-			if err != nil {
-				fmt.Println(err.Error())
-				return
-			}
-			
-			if len(model.ErrorLog) > 0 {
-				for _, e := range model.ErrorLog {
-					fmt.Println(e)
-				}
-			}
-		}()
-		if args.Mode == cli.ModeBackup {
-			core.Backup(settings, toViewQueue, toManagerQueue)
-		} else {
-			core.Restore(settings, toViewQueue, toManagerQueue)
-		}
-		wg.Wait()
-	}
-	
-	switch args.Mode {
-	case cli.ModeBackup:
-		run()
-	case cli.ModeRestore:
-		run()
-	case cli.ModeVersion:
-		fmt.Println(constants.APP_VERSION)
-	case cli.ModeHelp:
-		cli.Usage()
-	}
+	fmt.Printf("%s v%s", constants.APP_NAME, constants.APP_VERSION)
 }
