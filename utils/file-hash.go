c3e751ec59e076f5e68ee025ddb93fcf948a14d6
ブルー <98825268+nOjms-Blue@users.noreply.github.com>
2026-05-29 20:34:34 +0900

    feat: calculate hash value of file using SHA256

--- utils/file-hash.go
+++ utils/file-hash.go
@@ -1,10 +1,12 @@
 package utils
 
 import (
+	"crypto/sha256"
 	"encoding/binary"
 	"hash/crc32"
+	"io"
+	"os"
 )
-
 
 // データの CRC32(IEEE) を計算し、4バイト（BigEndian）で返す。
 func CRC32HashBytes(data []byte) []byte {
@@ -12,4 +14,20 @@ func CRC32HashBytes(data []byte) []byte {
 	buf := make([]byte, 4)
 	binary.BigEndian.PutUint32(buf, hash)
 	return buf
+}
+
+// ファイルのSHA256のハッシュ値を求める
+func CalcSHA256FromFile(file string) ([]byte, error) {
+	fp, err := os.Open(file)
+	if err != nil {
+		return []byte{}, err
+	}
+	defer fp.Close()
+	
+	hasher := sha256.New()
+	if _, err := io.Copy(hasher, fp); err != nil {
+		return []byte{}, err
+	}
+	
+	return hasher.Sum(nil), nil
 }
